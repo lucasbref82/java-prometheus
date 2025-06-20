9f40aaa133c991a296d8c019b6dc5a6b41fc2410
Lucas Brenner <lucasbref82@gmail.com>
2025-06-20 00:54:27 -0300

    Buscar todos

--- src/main/java/br/com/javaprometheus/controller/IndexController.java
+++ src/main/java/br/com/javaprometheus/controller/IndexController.java
@@ -7,17 +7,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @RestController
-@RequestMapping(value = "/usuario")
+@RequestMapping(value = "/usuarios")
 public class IndexController {
 
     @GetMapping
-    public ResponseEntity<String> init() {
-        return ResponseEntity.ok("Requisição feita com sucesso !");
+    public ResponseEntity<Set<Usuario>> buscarTodos() {
+        Usuario usuarioLucas = new Usuario.Builder().id(1L).nome("Lucas").login("lucasbref82").senha("root").build();
+        Usuario usuarioLarissa = new Usuario.Builder().id(2L).nome("Larissa").login("larissamurta").senha("1234").build();
+        Set<Usuario> usuarios = new HashSet<>(List.of(usuarioLucas, usuarioLarissa));
+        return ResponseEntity.ok(usuarios);
     }
 
     @GetMapping("/{nome}")
-    public ResponseEntity<Usuario> retornaUsuario(@PathVariable String nome) {
+    public ResponseEntity<Usuario> buscarPorNome(@PathVariable() String nome) {
         return ResponseEntity.ok(new Usuario.Builder().id(1L).nome(nome).login("root").senha("root").build());
     }
+
 }
